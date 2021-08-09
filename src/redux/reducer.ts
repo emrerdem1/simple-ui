a426f76556d1a9e796e6cae1d150bc03eafb8a0e
emre erdem <emrerdem94@gmail.com>
2021-08-09 08:28:34 +0300

    Set Redux boilerplate

---
+++ src/redux/reducer.ts
@@ -0,0 +1,35 @@
+import { combineReducers, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
+import { AuthenticationState, Language, ThemeColor, ThemeState } from './types';
+
+const initialStateLogin: AuthenticationState = {
+  user: null
+};
+
+const loginSlice = createSlice({
+  name: 'auth',
+  initialState: initialStateLogin,
+  reducers: {
+  },
+});
+
+const initialStateTheme: ThemeState = {
+  mode: ThemeColor.LIGHT,
+  language: Language.TR
+};
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState: initialStateTheme,
+  reducers: {
+  },
+});
+
+export const { actions: loginActions, reducer: loginReducer } = loginSlice;
+export const authentication = (state: RootState) => state.loginSlice;
+
+export const { actions: themeActions, reducer: themeReducer } = themeSlice;
+export const theme = (state: RootState) => state.themeSlice;
+
+const rootReducer = combineReducers({ loginSlice: loginReducer, themeSlice: themeReducer });
+export default rootReducer;
---
+++ src/redux/types.ts
@@ -0,0 +1,24 @@
+interface User {
+    name: string,
+    email: string,
+    password: string
+}
+
+export interface AuthenticationState {
+    user?: User,
+}
+
+export enum ThemeColor {
+    LIGHT,
+    DARK
+}
+
+export enum Language {
+    TR,
+    ENG
+}
+
+export interface ThemeState {
+    mode: ThemeColor,
+    language: Language
+}
